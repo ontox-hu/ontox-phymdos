2304e4fa2babafc769d9713e31bce2c0a1c20257
DanielRoodzantHU <daniel.roodzant@student.hu.nl>
2021-09-29 11:28:41 +0200

    progress for xml conversion

--- app/sbtab_to_sbml.py
+++ app/sbtab_to_sbml.py
@@ -5,29 +5,29 @@ from sbtab import validatorSBtab
 from sbtab import sbtab2sbml
 from pathlib import Path
 
-from SBtab import SBtabTable
-from validatorSBtab import ValidateTable
-from sbtab2sbml import SBtabDocument
+# from sbtab.SBtab import SBtabTable
+# from sbtab.validatorSBtab import ValidateTable
+# from sbtab.sbtab2sbml import SBtabDocument
 
 # Read in TSV file containing SBtab info and definitions file
-sbtab_file   = open("physmap.tsv")
+sbtab_file   = open(r.file_test,'r')
 file_content = sbtab_file.read()
 sbtab_file.close()
 
-def_file = open("definitions.tsv", "r")
-def_file_content = def_file.read()
-def_file.close()
+# def_file = open('/Users/danie/OneDrive/Documenten/R/Files/ontox-pmdep/definitions.tsv', "r")
+# def_file_content = def_file.read()
+# def_file.close()
 
 # Make SBtab object
 Sd = SBtab.SBtabDocument()
 Sd.set_filename('physmap.tsv')
 Sd.set_name('physmap')
-Sd.add_sbtab_string(file_content,filename='physmap.tsv')
+Sd.add_sbtab_string(file_content, filename='physmap.tsv')
 
-def_tabl = SBtab.SBtabDocument()
-def_tabl.set_filename('definitions.tsv')
-def_tabl.set_filename('definitions')
-Sd.add_sbtab_string(def_file_content,filename='definitions.tsv')
+# def_tabl = SBtab.SBtabDocument()
+# def_tabl.set_filename('definitions.tsv')
+# def_tabl.set_filename('definitions')
+# Sd.add_sbtab_string(def_file_content,filename='definitions.tsv')
 
 # Validate the SBtab file and save warnings
 V_obj = validatorSBtab.ValidateDocument(Sd) #, def_tabl
@@ -39,6 +39,8 @@ Cd = sbtab2sbml.SBtabDocument(Sd)
 (sbml, warnings) = Cd.convert_to_sbml('24')
 
 # Save SBML xml file (overwrites existing file with the same name)
-myfile = open("physmap.xml", "w")
+myfile = open("test_fromapp.xml", "w")
 myfile.write(sbml)
 myfile.close()
+
+#source_python('table_to_sbml.py')
