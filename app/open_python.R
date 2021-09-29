bd6d45419f17a4f8e5b9b84d42f0b0ec8114d1a6
DanielRoodzantHU <daniel.roodzant@student.hu.nl>
2021-09-29 11:28:41 +0200

    progress for xml conversion

--- app/open_python.R
+++ app/open_python.R
@@ -1,5 +1,12 @@
-# Open python in R and load sbtab converter
 library(reticulate)
+library(evaluate)
+library(here)
+library(rPython)
+
+# Open python in R and load sbtab converter
 conda_create("ontox-app", python_version = "3.8")
-conda_install("ontox-app", "sbtab", pip = TRUE)
+conda_install("ontox-app", "sbtab", pip = FALSE)
 use_condaenv("ontox-app", required = TRUE)
+
+# Load in file to convert
+file_test <- here::here('app', 'physmap.tsv')
